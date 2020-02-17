0827a455e49144f5beff47a3afb5037b25de18f1
guillaumeguirriec <guirriec.guillaume@gmail.com>
2020-02-17 15:37:56 +0100

    Add top 5 anomalies by type and sub-type

--- services/stats.js
+++ services/stats.js
@@ -9,7 +9,8 @@ exports.statsByArrondissement = async (client, callback) => {
             aggs: {
                 arrondissements: {
                     terms: {
-                        field: "arrondissement.keyword"
+                        field: "arrondissement.keyword",
+                        size: 20
                     }
                 }
             }
@@ -24,9 +25,44 @@ exports.statsByArrondissement = async (client, callback) => {
     callback(stat);
 }
 
-exports.statsByType = (client, callback) => {
-    // TODO Trouver le top 5 des types et sous types d'anomalies
-    callback([]);
+exports.statsByType = async (client, callback) => {
+    const response = await client.search({
+        index: indexName,
+        body: {
+            size: 0,
+            aggs: {
+                types: {
+                    terms: {
+                        field: "type.keyword",
+                        size: 5
+                    },
+                    aggs: {
+                        sous_types: {
+                            terms: {
+                                field: "sous_type.keyword",
+                                size: 5
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    const stat = response.body.aggregations.types.buckets.map(element => {
+        return {
+            "types": element.key,
+            "count": element.doc_count,
+            "sous_types": element.sous_types.buckets.map(sous_type => {
+                return {
+                    "sous_type": sous_type.key,
+                    "count": sous_type.doc_count
+                }
+            })
+        };
+    });
+
+    callback(stat);
 }
 
 exports.statsByMonth = (client, callback) => {
